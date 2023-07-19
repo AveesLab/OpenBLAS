d206f5d63afca52a9de15293a11f2f422fce3192
solahn <61405226+solahn@users.noreply.github.com>
2023-07-20 01:02:17 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 make TARGET=ARMV8
 sudo make PREFIX=/usr/local install
 cd
-# echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ".bashrc"
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ".bashrc"
 source .bashrc
 
 sudo apt-get install libv4l-dev
