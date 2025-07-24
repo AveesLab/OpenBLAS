4e657b98b4713fcde26c116f4695225539386ce2
Sol Ahn <61405226+solahn@users.noreply.github.com>
2025-07-24 11:33:38 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,18 @@
-make TARGET=ARMV8
-sudo make PREFIX=/usr/local install
-cd
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ".bashrc"
-source .bashrc
+#!/bin/bash
 
-sudo apt-get install libv4l-dev
+# 비밀번호 변수 설정
+PASSWORD="nvidia"
+
+# make 빌드
+make TARGET=ARMV8
+
+# sudo make install (비밀번호 자동 입력)
+echo "$PASSWORD" | sudo -S make PREFIX=/usr/local install
+
+# .bashrc 환경변수 설정
+cd
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib' >> ~/.bashrc
+source ~/.bashrc
+
+# libv4l-dev 자동 설치 (yes 자동 + 비밀번호 자동 입력)
+echo "$PASSWORD" | sudo -S apt-get install -y libv4l-dev
